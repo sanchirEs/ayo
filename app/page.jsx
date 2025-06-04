338441abcd9561ad0ccb67c14b4637b40a85564f
sanchirEs <sanchirenkhamgalan@gmail.com>
2025-06-04 20:52:11 +0800

    init

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import HomePage1 from "./(homes)/home-1/page";
+import HomePage15 from "./home/page";
+
+export const metadata = {
+  title: "Home 1 || Uomo eCommerce React Nextjs Template",
+  description: "Uomo eCommerce React Nextjs Template",
+};
+export default function Home() {
+  return (
+    <>
+      <HomePage15 />
+    </>
+  );
+}
---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  sassOptions: {
+    quietDeps: true, // This will silence deprecation warnings
+    silenceDeprecations: [
+      "import",
+      "global-builtin",
+      "color-functions",
+      "slash-div",
+      "mixed-decls",
+      "abs-percent",
+      "function-units",
+      "strict-unary",
+      "legacy-js-api",
+    ],
+  },
+};
+
+export default nextConfig;
